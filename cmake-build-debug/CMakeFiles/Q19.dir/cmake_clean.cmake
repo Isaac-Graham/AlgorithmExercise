58d68518bc06b2b2ffe7da0793a0e6ea269062ca
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-30 21:21:01 +0800

    Upload solution to question 19

---
+++ cmake-build-debug/CMakeFiles/Q19.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q19.dir/1-20/19_Remove_Nth_Node_from_End_of_List.cpp.o"
+  "Q19.pdb"
+  "Q19.exe"
+  "libQ19.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
