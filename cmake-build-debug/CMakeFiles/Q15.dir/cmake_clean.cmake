3101d2870df92e0422f3f50f685978846df86809
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-30 18:51:21 +0800

    Upload solution to quetsion 16

---
+++ cmake-build-debug/CMakeFiles/Q15.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q15.dir/1-20/15_3_sum.cpp.o"
+  "Q15.pdb"
+  "Q15.exe"
+  "libQ15.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Q16.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q16.dir/1-20/16_3_Sum_Closest.cpp.o"
+  "Q16.pdb"
+  "Q16.exe"
+  "libQ16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
