acd8464e4655e36b1d4755634fc9c91e9e97f5ea
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-30 21:33:11 +0800

    Upload solution to question 20

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,5 +50,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/main.dir/DependInfo.cmake"
-  "CMakeFiles/Q19.dir/DependInfo.cmake"
+  "CMakeFiles/Q20.dir/DependInfo.cmake"
   )
