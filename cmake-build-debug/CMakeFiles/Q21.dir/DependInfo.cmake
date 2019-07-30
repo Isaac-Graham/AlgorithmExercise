f2af98f2acff1e391b9914b6898acd2635c01c1d
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-30 22:24:25 +0800

    Upload the solution to question 21

---
+++ cmake-build-debug/CMakeFiles/Q21.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/e/Code/Leetcode/21-40/21_Merge_Two_Sorted_Lists.cpp" "/cygdrive/e/Code/Leetcode/cmake-build-debug/CMakeFiles/Q21.dir/21-40/21_Merge_Two_Sorted_Lists.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
