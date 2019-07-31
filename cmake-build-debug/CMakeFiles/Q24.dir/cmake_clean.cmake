2328f18126c3a7081340b87550056fda508ca76f
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-31 20:43:27 +0800

    Upload solution to question 24

---
+++ cmake-build-debug/CMakeFiles/Q24.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q24.dir/21-40/24_Swap_Nodes_in_Pairs.cpp.o"
+  "Q24.pdb"
+  "Q24.exe"
+  "libQ24.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
