8a3629ca76d5fac836082bfc47e137e11f6a0e02
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-07-31 19:03:37 +0800

    Upload solution to question 23

---
+++ cmake-build-debug/CMakeFiles/Q23.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q23.dir/21-40/23_Merge_k_Sorted_Lists.cpp.o"
+  "Q23.pdb"
+  "Q23.exe"
+  "libQ23.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
