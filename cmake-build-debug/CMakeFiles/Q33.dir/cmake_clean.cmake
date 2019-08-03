df4112bd058ef33aafecbf2cdceb2fe63bfa65b4
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-08-03 14:22:48 +0800

    Upload solutions to question 32-34

---
+++ cmake-build-debug/CMakeFiles/Q33.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q33.dir/21-40/33_Search_in_Rotated_Sorted_Array.cpp.o"
+  "Q33.pdb"
+  "Q33.exe"
+  "libQ33.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q33.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
