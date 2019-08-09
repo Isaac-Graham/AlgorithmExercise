83344eaa980dbf483a0c28bfb875f9129f508d07
Isaac_Chen <11712225@mail.sustech.edu.cn>
2019-08-09 18:56:41 +0800

    NULL

--- 21-40/37_Sudoku_Solver.cpp
+++ 21-40/37_Sudoku_Solver.cpp
@@ -7,9 +7,39 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<bool>> block_visit;
-    vector<vector<bool>> row_visit;
-    vector<vector<bool>> col_visit;
+    vector<vector<bool>> block_visit = {{false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+                                        {false, false, false, false, false, false, false, false, false, false},
+    };
+    vector<vector<bool>> row_visit = {{false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+    };
+    vector<vector<bool>> col_visit = {{false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+                                      {false, false, false, false, false, false, false, false, false, false},
+    };
     vector<vector<int>> v;
 
     inline int blockNum(int row, int column) {
@@ -18,32 +48,65 @@ public:
 
 // 回溯函数
     void back(int &r, int &c) {
+        int n = v[r][c];
+        v[r][c] = 0;
+        row_visit[r][n] = false;
+        col_visit[c][n] = false;
+        block_visit[blockNum(r, c)][n] = false;
+        c--;
         for (; c >= 0; c--) {
-
+            int num = v[r][c];
+            if (num < 0)
+                continue;
+            row_visit[r][num] = false;
+            col_visit[c][num] = false;
+            block_visit[blockNum(r, c)][num] = false;
+            if (num < 9) {
+                cout << "记录一下： " << endl;
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        cout << v[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                return;
+            }
+            v[r][c] = 0;
         }
+        r--;
+        for (c = 8; c >= 0; c--) {
+            for (; r >= 0; r--) {
+                int num = v[r][c];
+                if (num < 0)
+                    continue;
+                row_visit[r][num] = false;
+                col_visit[c][num] = false;
+                block_visit[blockNum(r, c)][num] = false;
+                if (num < 9) {
+                    cout << "记录一下： " << endl;
+                    for (int i = 0; i < 9; i++) {
+                        for (int j = 0; j < 9; j++) {
+                            cout << v[i][j] << " ";
+                        }
+                        cout << endl;
+                    }
+                    return;
+                }
+                v[r][c] = 0;
+            }
+        }
+
     }
 
     void solveSudoku(vector<vector<char>> &board) {
-//        vector<vector<bool>> block_visit;
-//        vector<vector<bool>> row_visit;
-//        vector<vector<bool>> col_visit;
-
-        for (int index = 0; index < 9; index++) {
-            block_visit.emplace_back(vector<bool>(10));
-            row_visit.emplace_back(vector<bool>(10));
-            col_visit.emplace_back(vector<bool>(10));
-        }
-
-
-//        vector<vector<int>> v;
         for (int r = 0; r < 9; r++) {
             vector<int> vec(9);
             for (int c = 0; c < 9; c++) {
                 vec[c] = board[r][c] == '.' ? 0 : -1 * ((int) board[r][c] - 48);
                 if (vec[c] != 0) {
-                    row_visit[r][vec[c]] = true;
-                    col_visit[c][vec[c]] = true;
-                    block_visit[blockNum(r, c)][vec[c]] = true;
+                    row_visit[r][-1 * vec[c]] = true;
+                    col_visit[c][-1 * vec[c]] = true;
+                    block_visit[blockNum(r, c)][-1 * vec[c]] = true;
                 }
             }
             v.push_back(vec);
@@ -52,23 +115,35 @@ public:
 
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                int num = v[row][col];
-                if (num == 0)
+                if (v[row][col] < 0)
                     continue;
-                for (int i = 1; i <= 9; i++) {
+                for (int num = v[row][col] + 1; num <= 9; num++) {
                     if (row_visit[row][num] || col_visit[col][num] || block_visit[blockNum(row, col)][num]) {
-                        if (i == 9) {
+                        if (num == 9) {
+//                            v[row][col] = 0;
                             // 回溯
+                            back(row, col);
+                            col--;
+                            break;
                         } else {
                             continue;
                         }
                     } else {
+                        v[row][col] = num;
                         row_visit[row][num] = true;
                         col_visit[col][num] = true;
                         block_visit[blockNum(row, col)][num] = true;
+                        break;
                     }
                 }
             }
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    cout << v[i][j] << " ";
+                }
+                cout << endl;
+            }
+            cout << "-----------------------------" << endl;
         }
     }
 
@@ -76,9 +151,20 @@ public:
 
 int main() {
     Solution *s = new Solution();
+//    vector<vector<char>> board = {
+//            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+//            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+//            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+//            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+//            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+//            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+//            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+//            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+//            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+//    };
     vector<vector<char>> board = {
-            {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
-            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'5', '3', '4', '6', '7', '8', '1', '9', '2'},
+            {'6', '7', '2', '1', '9', '5', '.', '.', '.'},
             {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
             {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
             {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
@@ -87,6 +173,13 @@ int main() {
             {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
             {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
     };
+    s->solveSudoku(board);
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
 
 
 }
